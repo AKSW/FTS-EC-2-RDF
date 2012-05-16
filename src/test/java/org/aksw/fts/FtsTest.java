5b58080e28a3123b623740a5e35c532f330304fe
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2012-05-16 17:07:39 +0200

    Initial test case

--- src/test/java/org/aksw/fts/FtsTest.java
+++ src/test/java/org/aksw/fts/FtsTest.java
@@ -3,6 +3,51 @@ package org.aksw.fts;
 import java.io.File;
 
 import org.junit.Test;
+import org.openjena.atlas.lib.Sink;
+
+import com.hp.hpl.jena.graph.Triple;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.sparql.util.ModelUtils;
+
+class SinkModel
+	implements Sink<Triple>
+{
+	private Model model;
+	
+	public SinkModel() {
+		this.model = ModelFactory.createDefaultModel();
+	}
+	
+	public SinkModel(Model model) {
+		this.model = model;
+	}
+	
+	public Model getModel() {
+		return model;
+	}
+	
+
+	@Override
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void flush() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void send(Triple triple) {
+		Statement stmt = ModelUtils.tripleToStatement(model, triple);
+		model.add(stmt);		
+	}
+	
+}
 
 public class FtsTest {
 	
@@ -10,8 +55,12 @@ public class FtsTest {
 	public void test()
 		throws Exception
 	{
-		File sourceFile = new File("src/main/test/resources/fts-exceprt-2009-en.xml");
+		SinkModel sink = new SinkModel();
 		
-		Main.process(sourceFile);
+		File sourceFile = new File("src/test/resources/fts-excerpt-2009-en.xml");
+		
+		Main.process(sourceFile, sink);
+		
+		sink.getModel().write(System.out, "TTL");
 	}
 }
